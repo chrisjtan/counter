f85899ccc614fb081e54ee3313609e9de37894bd
Juntao Tan <hust.jtt@gmail.com>
2021-11-25 00:24:55 -0500

    update argument file

--- utils/argument.py
+++ utils/argument.py
@@ -1,7 +1,8 @@
 import argparse
 
-def arg_parser_pre_processing_yelp():
+def arg_parser_pre_processing():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", dest="dataset", type=str, default="yelp")
     parser.add_argument("--sentires_dir", dest="sentires_dir", type=str, default="./datasets/Yelp/reviews.pickle", 
                         help="path to pre-extracted sentires data")
     parser.add_argument("--review_dir", dest="review_dir", type=str, default="./datasets/Yelp/yelp_academic_dataset_review.json", 
@@ -18,4 +19,17 @@ def arg_parser_pre_processing_yelp():
     help="num of sampled items in evaluation")
     parser.add_argument("--save_path", dest="save_path", type=str, default="./dataset_objs/", 
     help="The path to save the preprocessed dataset object")
+    return parser.parse_args()
+
+
+def arg_parse_train_base():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", dest="dataset", type=str, default="yelp")
+    parser.add_argument("--gpu", dest="gpu", action="store_false", help="whether to use gpu")
+    parser.add_argument("--cuda", dest="cuda", type=str, default='0', help="which cuda")
+    parser.add_argument("--weight_decay", dest="weight_decay", type=float, default='1e-5', help="L2 norm to the wights")
+    parser.add_argument("--lr", dest="lr", type=float, default=0.0005, help="learning rate for training")
+    parser.add_argument("--epoch", dest="epoch", type=int, default=1000, help="training epoch")
+    parser.add_argument("--batch_size", dest="batch_size", type=int, default=256, help="batch size for training base rec model")
+    parser.add_argument("--embedding_length", dest="embedding_length", type=int, default=256, help="implicit feature length")
     return parser.parse_args()
