84c586e2f7351bae28231c7e0aacf2e923b13c49
Juntao Tan <hust.jtt@gmail.com>
2021-12-01 21:46:32 -0800

    add train base model

---
+++ scripts/train_base.py
@@ -0,0 +1,91 @@
+import torch
+import numpy as np
+import os
+import tqdm
+import pickle
+from pathlib import Path
+from torch.utils.data import DataLoader
+from scripts.preprocessing.dataset_init import dataset_init
+from utils.argument import arg_parse_train_base, arg_parser_preprocessing
+from models.data_loaders import UserItemInterDataset
+from models.models import BaseRecModel
+from utils.evaluate_functions import compute_ndcg
+
+
+def train_base_recommendation(train_args, pre_processing_args):
+    if train_args.gpu:
+        device = torch.device('cuda:%s' % train_args.cuda)
+    else:
+        device = 'cpu'
+
+    # # if first time run this code, uncomment below part
+    # rec_dataset = dataset_init(pre_processing_args)
+    # Path(pre_processing_args.save_path).mkdir(parents=True, exist_ok=True)
+    # with open(os.path.join(pre_processing_args.save_path, "yelp_dataset_obj.pickle"), 'wb') as outp:
+    #     pickle.dump(rec_dataset, outp, pickle.HIGHEST_PROTOCOL)
+    # # end
+
+    with open(os.path.join(pre_processing_args.save_path, train_args.dataset + "_dataset_obj.pickle"), 'rb') as inp:
+        rec_dataset = pickle.load(inp)
+
+    train_loader = DataLoader(dataset=UserItemInterDataset(rec_dataset.training_data, 
+                                rec_dataset.user_feature_matrix, 
+                                rec_dataset.item_feature_matrix),
+                          batch_size=train_args.batch_size,
+                          shuffle=True)
+
+    model = BaseRecModel(rec_dataset.feature_num).to(device)
+    loss_fn = torch.nn.BCELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=train_args.lr, weight_decay=train_args.weight_decay)
+
+    out_path = os.path.join("./logs", train_args.dataset + "_logs")
+    Path(out_path).mkdir(parents=True, exist_ok=True)
+
+    ndcg = compute_ndcg(rec_dataset.test_data, 
+            rec_dataset.user_feature_matrix, 
+            rec_dataset.item_feature_matrix, 
+            train_args.rec_k, 
+            model, 
+            device)
+    print('init ndcg:', ndcg)
+    for epoch in tqdm.trange(train_args.epoch):
+        model.train()
+        optimizer.zero_grad()
+        losses = []
+        for user_behaviour_feature, item_aspect_feature, label in train_loader:
+            user_behaviour_feature = user_behaviour_feature.to(device)
+            item_aspect_feature = item_aspect_feature.to(device)
+            label = label.float().to(device)
+            out = model(user_behaviour_feature, item_aspect_feature).squeeze()
+            loss = loss_fn(out, label)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            losses.append(loss.to('cpu').detach().numpy())
+            ave_train = np.mean(np.array(losses))
+        print('epoch %d: ' % epoch, 'training loss: ', ave_train)
+        # compute necg
+        if epoch % 10 == 0:
+            ndcg = compute_ndcg(rec_dataset.test_data, 
+            rec_dataset.user_feature_matrix, 
+            rec_dataset.item_feature_matrix, 
+            train_args.rec_k, 
+            model, 
+            device)
+            print('epoch %d: ' % epoch, 'training loss: ', ave_train, 'NDCG: ', ndcg)
+    torch.save(model.state_dict(), os.path.join(out_path, "model.model" % epoch))
+    return 0
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    np.random.seed(0)
+    t_args = arg_parse_train_base()  # training arguments
+    p_args = arg_parser_preprocessing()  # pre processing arguments
+    if t_args.gpu:
+        os.environ["CUDA_VISIBLE_DEVICES"] = t_args.cuda
+        print("Using CUDA", t_args.cuda)
+    else:
+        print("Using CPU")
+    print(p_args)
+    train_base_recommendation(t_args, p_args)
